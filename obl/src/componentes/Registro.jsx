5662c4716311b853bf941f07b508d8815556a6f2
Gianfranco Bonanni <110034721+GianfrancoB23@users.noreply.github.com>
2024-08-07 22:20:19 -0300

    registro 99%

--- obl/src/componentes/Registro.jsx
+++ obl/src/componentes/Registro.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useId, useReducer, useRef } from 'react'
+import React, { useEffect, useId, useReducer, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux'
 import { guardarDepartamentos } from '../features/departamentosSlice';
+import { guardarCiudades } from '../features/ciudadesSlice';
 
 
 const Registro = () => {
@@ -20,15 +21,36 @@ const Registro = () => {
 
     const dispatch = useDispatch();
     const departamentos = useSelector(state => state.departamentos.departamentos)
+    const [departamentoSeleccionado, setDepartamentoSeleccionado] = useState('');
     const ciudades = useSelector(state => state.ciudades.ciudades)
+
+    let campo = useRef(null)
+
 
     useEffect(() => {
         fetch("https://babytracker.develotion.com/departamentos.php")
             .then(r => r.json())
             .then(datos => {
-                dispatch(guardarDepartamentos(datos))
+                console.log(datos);
+
+                dispatch(guardarDepartamentos(datos.departamentos))
             })
     }, [])
+
+    const cargarCity = (e) => {
+        setDepartamentoSeleccionado(e.target.value);
+    };
+
+    useEffect(() => {
+        if (departamentoSeleccionado) {
+            fetch(`https://babytracker.develotion.com/ciudades.php?idDepartamento=${departamentoSeleccionado}`)
+                .then(r => r.json())
+                .then(datos => {
+                    console.log("Ciudades", datos.ciudades);
+                    dispatch(guardarCiudades(datos.ciudades));
+                })
+        }
+    }, [departamentoSeleccionado, dispatch]);
 
     const registrar = () => {
         const userCampoReg = userCampoReg.current.value;
@@ -56,13 +78,13 @@ const Registro = () => {
             <input type="password" className="form-control" id={idPassReg} placeholder="Ingrese su contraseña" ref={passCampoReg} />
 
             <label htmlFor={idSlcDpto}>Departamento</label>
-            <select className="form-control" id={idSlcDpto} ref={slcDptoCampo}>
-                {departamentos.map(departamento => <option key={departamento.id} {...departamento}/>)}
+            <select className="form-control" id={idSlcDpto} ref={slcDptoCampo} onChange={cargarCity}>
+                {departamentos.map(departamento => <option key={departamento.id} value={departamento.id} >{departamento.nombre}</option>)}
             </select>
 
             <label htmlFor={idSlcCity}>Ciudad</label>
             <select className="form-control" id={idSlcCity} ref={slcCityCampo}>
-                {/* OPCIONES */}
+                {ciudades.map(ciudad => <option key={ciudad.id} value={ciudad.id}>{ciudad.nombre}</option>)}
             </select>
 
             <input type="button" value="REGISTRAR" onClick={registrar} className='mt-2' />
--- obl/src/store/store.js
+++ obl/src/store/store.js
@@ -1,9 +1,10 @@
-import { configureStore } from "@reduxjs/toolkit"
-import departamentosReducer from "../features/departamentosSlice"
+import { configureStore } from '@reduxjs/toolkit';
+import departamentosReducer from '../features/departamentosSlice';
+import ciudadesReducer from '../features/ciudadesSlice';
 
 export const store = configureStore({
     reducer: {
         departamentos: departamentosReducer,
-    }
-})
-
+        ciudades: ciudadesReducer
+    },
+});
